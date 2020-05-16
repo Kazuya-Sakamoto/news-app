592b9616ed70df8f2ab484e292998fc73405aa74
Kazuya Sakamoto <56709557+Kazuya-Sakamoto@users.noreply.github.com>
2020-05-16 18:08:47 +0900

    Article Screen Create

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../screens/HomeScreen'
+import ArticleScreen from '../screens/ArticleScreen'
+
 
 const Stack = createStackNavigator();
 
@@ -14,6 +16,10 @@ export default AppNavigator = () => {
           name="Home"
           component={ HomeScreen }
           options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Article"
+          component={ ArticleScreen }
         />
       </Stack.Navigator>
     </NavigationContainer>
