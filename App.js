fcfd3c18f05334a015e565bf74270dcaf7e5e176
Kazuya Sakamoto <56709557+Kazuya-Sakamoto@users.noreply.github.com>
2020-05-16 14:31:44 +0900

    install axios

--- App.js
+++ App.js
@@ -3,6 +3,10 @@ import { StyleSheet, FlatList, SafeAreaView } from 'react-native';
 import ListItem from './components/ListItem'; //* component の import
 import dummyArticles from './dummies/article.json'  //* dummyData を取得
 import Constants from 'expo-constants'
+import axios from 'axios'
+
+const URL = `https://newsapi.org/v2/top-headlines?country=jp&apiKey=${Constants.manifest.extra.newsApiKey}`;
+
 
 const styles = StyleSheet.create({
   container: {
@@ -14,12 +18,23 @@ const styles = StyleSheet.create({
 export default function App() {
   const [articles, setArticles] = useState(); //*state　導入
   useEffect(() => { //* useEffect の導入
-    const timer = setTimeout(() => { 
-      // alert(Constants.manifest.extra.newsApiKey) //! API KEY がとれてるか alert で確認してみる
-      setArticles(dummyArticles)
-    }, 2000);
-    return () => clearTimeout(timer) //* timer をクリーンアップする
+    fetchArticle();
+    // const timer = setTimeout(() => { 
+    // alert(Constants.manifest.extra.newsApiKey) //! API KEY がとれてるか alert で確認してみる
+    //   setArticles(dummyArticles)
+    // }, 2000);
+    // return () => clearTimeout(timer) //* timer をクリーンアップする
   }, []) //* 空の配列を渡すとマウント時のみ発火する設定になる
+
+
+  const fetchArticle = async () => {
+    try {
+      const response = await axios.get(URL);
+      setArticles(response.data.articles);
+    } catch (error) {
+      console.error(error);
+    }
+  }
   return (
     <SafeAreaView style={styles.container}>
     <FlatList
