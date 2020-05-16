56d2af5a8da0859baaf77d4c209be35806b4e981
Kazuya Sakamoto <56709557+Kazuya-Sakamoto@users.noreply.github.com>
2020-05-16 13:39:16 +0900

    hook import / useState, useEffect

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, FlatList, SafeAreaView } from 'react-native';
 import ListItem from './components/ListItem';
-import articles from './dummies/article.json'
+import dummyArticles from './dummies/article.json'
 
 const styles = StyleSheet.create({
   container: {
@@ -11,10 +11,17 @@ const styles = StyleSheet.create({
 });
 
 export default function App() {
+  const [articles, setArticles] = useState(); //*state　導入
+  useEffect(() => { //* useEffect の導入
+    const timer = setTimeout(() => { 
+      setArticles(dummyArticles)
+    }, 2000);
+    return () => clearTimeout(timer) //* timer をクリーンアップする
+  }, []) //* 空の配列を渡すとマウント時のみ発火する設定になる
   return (
     <SafeAreaView style={styles.container}>
     <FlatList
-      data={articles} //dataの配列を入れる
+      data={articles} //*dataの配列を入れる
       renderItem={({ item }) =>( 
         <ListItem
           imageUrl={item.urlToImage}
