033c735553e0e452270e79c4386361a47cd05158
Kazuya Sakamoto <56709557+Kazuya-Sakamoto@users.noreply.github.com>
2020-05-16 20:03:54 +0900

    create action && reducer

---
+++ store/actions/user.js
@@ -0,0 +1,15 @@
+// ! ADD CLIP Action
+export const addClip = ({ clip }) => {
+  return{
+    type: 'ADD_CLIP',
+    clip
+  }
+}
+
+// ! DELETE CLIP Action
+export const addClip = ({ clip }) => {
+  return{
+    type: 'DELETE_CLIP',
+    clip
+  }
+}
