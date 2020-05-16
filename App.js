d1936b39fe25508babcb6fb92048b91c27a639f5
Kazuya Sakamoto <56709557+Kazuya-Sakamoto@users.noreply.github.com>
2020-05-16 13:13:04 +0900

    Edit.css

--- App.js
+++ App.js
@@ -8,40 +8,9 @@ const styles = StyleSheet.create({
     flex: 1, //* resetCss 同様画面いっぱい 
     backgroundColor: '#fff',
   },
-  itemContainer: {
-    height: 100,
-    width: '100%',
-    borderColor: 'gray',
-    borderWidth: 1,
-    flexDirection: "row" //*横並び  //*column 縦並び 
-  },
-  leftContainer: {
-    width: 100
-  },
-  rightContainer: {
-    flex: 1, //* スペースを使い切る
-    padding: 10, 
-    justifyContent: "space-between", //* 間を開ける 
-  },
-  text:{
-    fontSize: 16
-  },
-  subText: {
-    fontSize: 12,
-    color: 'gray'
-  }
 });
 
 export default function App() {
-  // ? 一覧表示 変数
-  // const items = articles.map((article, index) => {return(
-  // <ListItem
-  //   imageUrl={article.urlToImage}
-  //   title={article.title}
-  //   author={article.author}
-  //   key={index}
-  // />
-  // )})
   return (
     <SafeAreaView style={styles.container}>
     <FlatList
