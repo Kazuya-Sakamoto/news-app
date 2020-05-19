b29898862a44721349d180b334332def81916e4b
Kazuya Sakamoto <56709557+Kazuya-Sakamoto@users.noreply.github.com>
2020-05-19 21:35:37 +0900

    useSelector Create-Clipbtn

--- screens/ClipScreen.js
+++ screens/ClipScreen.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { StyleSheet, SafeAreaView, Text } from 'react-native';
+import { useSelector } from "react-redux"
+import { FlatList } from 'react-native-gesture-handler';
+import ListItem from '../components/ListItem';
 
 const styles = StyleSheet.create({
   container: {
@@ -8,10 +11,25 @@ const styles = StyleSheet.create({
   },
 })
 
-export default ClipScreen = () => {
+export default ClipScreen = ({navigation}) => {
+  const user = useSelector(state => state.user)
+  const { clips } = user;
   return (
     <SafeAreaView style={ styles.container }>
-      <Text>Clip Screen</Text>
+      <FlatList
+        data={ clips }
+        renderItem={({ item }) => (
+          <ListItem
+            imageUrl={ item.urlToImage }
+            title={ item.title }
+            author={ item.author }
+            onPress={() => 
+              navigation.navigate("Article", { article: item })
+            } //*component.name
+          />
+        )}
+        keyExtractor={(item, index) => index.toString()}
+      />
     </SafeAreaView>
   );
 };
